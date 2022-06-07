a5dcd3cfd701fe176d0b5628a648dda83d46df66
bababuuui <bubinatar@gmail.com>
2022-06-07 21:57:15 +0200

    feat - args parsing

---
+++ src/args/parseArgs.js
@@ -0,0 +1,12 @@
+const getEnvVars = () => {
+   return  process.argv.slice(2);
+}
+
+export const getEnvVarValueByName = (name)=> {
+    const resultVar=  getEnvVars().find( (variable)=>
+    {
+        return variable.slice(2).split("=")[0] ===name;
+    }
+    );
+    return resultVar.slice(2).split("=")[1]
+}
