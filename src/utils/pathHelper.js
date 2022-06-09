e6a5687372a17877ad06f235f1a3190791936cdf
bababuuui <bubinatar@gmail.com>
2022-06-09 21:48:03 +0200

    fix - pathHelper

--- src/utils/pathHelper.js
+++ src/utils/pathHelper.js
@@ -1,25 +1,17 @@
 import path from "path";
-import {OPERATION_FAILED} from "../constants/messagesToUser.js";
 import CurrentFolder from "./currentFolder.js";
-import {isPathValid} from "./pathCheck.js";
 
 
 export const getResolvedPath = (desiredPath) => {
     let resultPath;
 
-    if(!desiredPath){
-        console.error(OPERATION_FAILED);
+    if(desiredPath){
+        if(path.isAbsolute(desiredPath)){
+            resultPath= desiredPath;
+        }
+        else{
+            resultPath= path.join(CurrentFolder.get(),desiredPath);
+        }
     }
-    if(path.isAbsolute(desiredPath)){
-        resultPath= desiredPath;
-    }
-    else{
-        resultPath= path.join(CurrentFolder.get(),desiredPath);
-    }
-
-    if(!isPathValid(resultPath)){
-        throw  new Error(OPERATION_FAILED);
-    }
-
     return resultPath;
 }
